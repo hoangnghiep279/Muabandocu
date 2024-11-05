f818d0446d8b151b2ff09cff20216b6628cea82c
hoangnghiep279 <133936067+hoangnghiep279@users.noreply.github.com>
2024-11-05 18:59:11 +0700

    fix main

--- muabandocu_client/src/main.jsx
+++ muabandocu_client/src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <BrowserRouter>
-            <App />
-        </BrowserRouter>
-  </StrictMode>,
-)
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
